2f8f8f67c3fc6c88e91e1a3c5e74a42cbb71dde5
junki57 <85267609+junki57@users.noreply.github.com>
2021-07-01 18:23:33 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    # validates :encrypted_password, presence: true, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字を両方含む'}
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ , message: '全角（漢字・ひらがな・カタカナ）での入力が必須'} do
+      validates :first_name
+      validates :family_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい。'} do
+      validates :first_name_kana 
+      validates :family_name_kana
+    end
+    validates :birthday
+  end
 end
