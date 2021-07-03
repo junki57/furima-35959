2d310d06a9a596e43cebc09f0390b4308bbb7066
junki57 <85267609+junki57@users.noreply.github.com>
2021-07-03 19:03:51 +0900

    モデルとテーブルを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,19 @@ class ItemsController < ApplicationController
 
 
   def index
+    @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params) 
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category_id, :item_condition_id, :postage_id, :prefecture_id, :shopping_date_id, :price).merge(user_id: current_user.id)
+  end
 end
