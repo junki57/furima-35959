1899b66e952e139490e823f2e4bc10585bf49ae1
junki57 <85267609+junki57@users.noreply.github.com>
2021-07-08 19:45:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :aaa, only: [:edit, :update]
+  before_action :user_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path
     else
       render :edit
     end
@@ -45,9 +45,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def aaa
+  def user_confirmation
     if current_user.id != @item.user.id
       redirect_to root_path 
-   end
+    end
   end
 end
